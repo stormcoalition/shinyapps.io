72dee0352737924320b247ec6d1f5ace26db5513
maseology <maseology@pm.me>
2023-12-10 13:47:48 -0500

    u

--- ORMCA/srv/leaflet.R
+++ ORMCA/srv/leaflet.R
@@ -1,9 +1,9 @@
 
-ormcp <- readOGR("https://raw.githubusercontent.com/stormcoalition/geojson/main/Oak_Ridges_Moraine_(ORM)_Land_Use_Designation.geojson")
+ormcp <- st_read("https://raw.githubusercontent.com/stormcoalition/geojson/main/Oak_Ridges_Moraine_(ORM)_Land_Use_Designation.geojson")
 
 ormcpPal <- colorFactor(
   c('#7fc97f','#beaed4','#fdc086','#ffff99','#386cb0','#f0027f'),
-  domain = ormcp@data$LAND_USE_DESIGNATION
+  domain = ormcp$LAND_USE_DESIGNATION
 )
 
 output$map <- renderLeaflet({
